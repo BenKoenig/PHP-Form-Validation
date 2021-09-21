69c7f70a590310f3a804eeed3ac1710c033b9a35
Benjamin König <bkoenig.as@gmail.com>
2021-09-21 12:03:14 +0200

    created base files

---
+++ contents/form.php
@@ -0,0 +1,26 @@
+<form>
+    <div>
+        <label for="firstName">First Name</label>
+        <input type="text" for="firstName" id="firstName">
+    </div>
+
+    <div>
+        <label for="lastName">Last Name</label>
+        <input type="text" for="lastName" id="lastName">
+    </div>
+
+    <div>
+        <label for="paymentMethod">Payment Method</label>
+        <select name="paymentMethod" id="paymentMethod">
+            <option value="_default" disabled selected>Please select a method</option>
+            <option value="paypal">PayPal</option>
+            <option value="googlepay">Google Pay</option>
+            <option value="mastercard">MasterCard</option>
+            <option value="visa">Visa</option>
+            a
+        </select>
+    </div>
+
+
+
+</form>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+    require_once 'partials/header.php';
+    require_once 'contents/form.php';
+    require_once 'partials/footer.php';
+?>
