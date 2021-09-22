497792dd374e96403c3771a4a77743d5b7c8849b
Benjamin König <bkoenig.as@gmail.com>
2021-09-22 12:00:25 +0200

    added phone & age validation, radio checked & checkbox selected now get saved on refresh, added TOS page

--- contents/form.php
+++ contents/form.php
@@ -26,10 +26,19 @@ if(isset($_POST['do-submit'])) {
     if (!isset($_POST['paymentMethod']) || $_POST['paymentMethod'] === '_default') {
         $errors['paymentMethod'] = 'Please select a payment method.';
     }
+    if(
+        $_POST['age'] > 118
+        || $_POST['age'] < -1
+    ) {
+        $errors['age'] = "Please enter a valid age.";
+    }
     if(!isset($_POST['tos'])){
         $errors['tos'] = "You must accept the terms of service";
     }
- 
+    $pattern = '/(\+?|0{0,2})[0-9 ]{1,3}[0-9 \/()-]+/m';
+    if (!isset($_POST['phone']) || preg_match($pattern, $_POST['phone']) !== 1) {
+        $errors['phone'] = 'Please enter a valid phone number.';
+    }
 }
 
 function printError (string $name) {
@@ -45,6 +54,8 @@ function old (string $name) {
     }
 }
 
+
+
 ?>
 
 <?php
@@ -54,26 +65,24 @@ function old (string $name) {
         <div class="form__field">
             <p>Select your gender</p>
             <fieldset>
-                <div>
-                    <input type="radio" id="female" name="gender" value="female">
-                    <label for="female">Female</label>
-                </div>
-                <div>
-                    <input type="radio" id="male" name="gender" value="male">
-                    <label for="male">Male</label>
-                </div>
-                <div>
-                    <input type="radio" id="non" name="gender" value="non">
-                    <label for="non">Non-binary</label>
-                </div>
-                <div>
-                    <input type="radio" id="other" name="gender" value="other">
-                    <label for="other">Other</label>
-                </div>
-                <div>
-                <?php
-                printError("gender");
-                ?>
+            <div>
+                <input type="radio" name="gender" id="f" value="[f]" <?php if(isset($_POST['gender']) && $_POST['gender'] =='[f]' ){echo "checked";}?> required>
+                <label for="f">Female</label>
+            </div>
+            <div>
+                <input type="radio" name="gender" id="m" value="[m]"  <?php if(isset($_POST['gender']) && $_POST['gender'] =='[m]' ){echo "checked";}?> required>
+                <label for="m">Male</label>
+            </div>
+            <div>
+                <input type="radio" name="gender" id="n" value="[n]"  <?php if(isset($_POST['gender']) && $_POST['gender'] =='[n]' ){echo "checked";}?> required> 
+                <label for="n">Non-binary</label>
+            </div>
+            <div>
+                <input type="radio" name="gender" id="o" value="[o]"  <?php if(isset($_POST['gender']) && $_POST['gender'] =='[o]' ){echo "checked";}?> required>
+                <label for="o">Other</label>    
+            </div>
+            <div>
+                <?php printError("gender");?>
             </div>
 
             </fieldset>
@@ -105,16 +114,28 @@ function old (string $name) {
             <label for="paymentMethod">Payment Method</label>
             <select name="paymentMethod" id="paymentMethod">
                 <option value="_default" disabled selected>Please select a method</option>
-                <option value="paypal">PayPal</option>
-                <option value="googlepay">Google Pay</option>
-                <option value="mastercard">MasterCard</option>
-                <option value="visa">Visa</option>
+                <option value="paypal" value="paypal" <?php if(isset($_POST['paymentMethod']) && $_POST['paymentMethod'] =='paypal' ){echo "selected";}?>>PayPal</option>
+                <option value="googlepay" value="googlepay" <?php if(isset($_POST['paymentMethod']) && $_POST['paymentMethod'] =='googlepay' ){echo "selected";}?>>Google Pay</option>
+                <option value="mastercard" value="mastercard" <?php if(isset($_POST['paymentMethod']) && $_POST['paymentMethod'] =='mastercard' ){echo "selected";}?>>MasterCard</option>
+                <option value="visa" value="visa" <?php if(isset($_POST['paymentMethod']) && $_POST['paymentMethod'] =='visa' ){echo "selected";}?>>Visa</option>
             </select>
             <div>
                 <?php printError("paymentMethod"); ?>
             </div>
-
-
+        </div>
+        <div class="form__field">
+            <label for="age">Your Age</label>
+            <input type="textfield" name="age" id="age" value="<?php old('age') ?>">
+            <div>
+                <?php printError("age"); ?>
+            </div>
+        </div>
+        <div class="form__field">
+            <label for="phone">Your Phone number</label>
+            <input type="text" name="phone" id="phone" value="<?php old('phone') ?>">
+            <div>
+                <?php printError("phone"); ?>
+            </div>
         </div>
         <div class="form__field">
             <label for="adress1">Adress Line 1</label>
@@ -126,8 +147,8 @@ function old (string $name) {
         </div>
         <div class="form__field">
             <div>
-                <input type="checkbox" name="tos" id="tos">
-                <label for="tos">I accept the <a href="#">terms of service</a></label>
+                <input type="checkbox" name="tos" id="tos" value="tos" <?php if(isset($_POST['tos']) && $_POST['tos'] =='tos' ){echo "checked";}?> >
+                <label for="tos">I accept the <a href="index.php?page=tos">terms of service</a></label>
             </div>
             <div>
                 <?php printError("tos"); ?>
